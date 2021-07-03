40b4ed6fd3416fbc9a7c7a680291f5fee9a156b9
Kelvin <laikelvin8@gmail.com>
2021-07-03 09:50:24 +0100

    Changed text + Added image + Imported Image + added handlePress function

--- App.js
+++ App.js
@@ -1,12 +1,20 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View,Image} from 'react-native';
 
 export default function App() {
+  const handlePress = () => console.log("Text pressed");
   return (
     <View style={styles.container}>
       <Text>Open up App.js to start working on your app!</Text>
+      <Text numberOfLines={1}>This is my first react project. I hope you guys enjoy this app</Text>
       <StatusBar style="auto" />
+      <Image
+      source={{
+        width:200,
+        height:300,
+        uri:"https://picsum.photos/200/300"}}
+        style={{top: 10,}}></Image>
     </View>
   );
 }

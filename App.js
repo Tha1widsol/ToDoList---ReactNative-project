45458339f4a09ab9eba3542b3d464453c9f709a4
Kelvin <laikelvin8@gmail.com>
2021-07-06 08:13:10 +0100

    Added button, touchables and more functionalities to App.js

--- App.js
+++ App.js
@@ -1,21 +1,28 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View,Image} from 'react-native';
+import { StyleSheet, Text, View,SafeAreaView,Image,TouchableOpacity,Button} from 'react-native';
 
 export default function App() {
   const handlePress = () => console.log("Text pressed");
   return (
-    <View style={styles.container}>
+    <SafeAreaView style={styles.container}>
       <Text>Open up App.js to start working on your app!</Text>
       <Text numberOfLines={1}>This is my first react project. I hope you guys enjoy this app</Text>
+      <SafeAreaView style={{top:20,}}>
+      <Button color ="red" title="new button"  onPress = {() => alert("Tapped")}></Button>
+      </SafeAreaView>
       <StatusBar style="auto" />
+  
+      <TouchableOpacity onPress = {()=>console.log("Tapped") }>
       <Image
       source={{
         width:200,
         height:300,
         uri:"https://picsum.photos/200/300"}}
-        style={{top: 10,}}></Image>
-    </View>
+        style={{top: 30,}}></Image>
+      </TouchableOpacity>
+     
+    </SafeAreaView>
   );
 }
 
@@ -25,5 +32,6 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
+    paddingBottom:"20",
   },
 });

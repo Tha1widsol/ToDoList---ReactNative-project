4408b127e7b1e133c27f6673b8886f027db4d33a
Kelvin <laikelvin8@gmail.com>
2021-07-21 07:25:45 +0100

    Added alerts + keybaourd dismiss handlers

--- App.js
+++ App.js
@@ -1,6 +1,6 @@
 import { StatusBar } from 'expo-status-bar';
 import React,{useState} from 'react';
-import { StyleSheet, Text, View,SafeAreaView,Image,TouchableOpacity,Button,FlatList} from 'react-native';
+import { StyleSheet, Text, View,SafeAreaView,Image,TouchableOpacity,Button,FlatList,Alert,TouchableWithoutFeedback,Keyboard} from 'react-native';
 import Header from './components/header';
 import ToDoitem from './components/todoitem';
 import AddTodo from './components/addTodo';
@@ -21,15 +21,27 @@ export default function App() {
   }
 
   const submitHandler = (text) => {
-    setTodos((prevTodos)=>{
-      return [
-        {text:text,key : Math.random().toString()},
-        ...prevTodos
-      ]
-    })
+    if(text.length > 5){
+      setTodos((prevTodos)=>{
+        return [
+          {text:text,key : Math.random().toString()},
+          ...prevTodos
+        ]
+      })
+    }
+    else{
+      Alert.alert("Invalid","ToDos must have a length of atleast 6",[
+        {text:"understood",onPress:() => console.log('alert closed')}
+      ]);
+    }
+   
   }
 
   return (
+    <TouchableWithoutFeedback onPress = {() => {
+      Keyboard.dismiss();
+    
+    }}>
     <SafeAreaView style={styles.container}>
       <Header title="ToDoList"></Header>
       <View style={styles.content}>
@@ -45,6 +57,7 @@ export default function App() {
         </View>
       </View>
     </SafeAreaView>
+    </TouchableWithoutFeedback>
 
     
   );

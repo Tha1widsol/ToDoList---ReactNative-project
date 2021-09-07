18a6a38a2f73c156e2e7337351293faac8f6858d
Kelvin <laikelvin8@gmail.com>
2021-09-07 22:07:57 +0100

    Added NavigationContainer component call

--- App.js
+++ App.js
@@ -1,13 +1,14 @@
 import React,{useState} from 'react';
-import { Button, SafeAreaView, StyleSheet, TextInput } from 'react-native'
+import { Button, SafeAreaView, StyleSheet, TextInput,Text } from 'react-native'
 import Header from './components/Header';
 import Navigator from './routes/homeStack';
+import { NavigationContainer } from '@react-navigation/native';
 
 export default function App() {
   return (
-    <>
+    <NavigationContainer>
     <Header title="ToDoApp"/>
     <Navigator/>
-   </>
+   </NavigationContainer>
   )
 }

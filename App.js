3a1025aacf636e3c35860ed3babe55588259dc50
Kelvin <laikelvin8@gmail.com>
2021-09-07 15:36:57 +0100

    Imported screens

--- App.js
+++ App.js
@@ -1,7 +1,6 @@
 import React,{useState} from 'react';
 import { Button, SafeAreaView, StyleSheet, TextInput } from 'react-native'
 import Header from './components/Header';
-import Home from './screens/Home';
 import Navigator from './routes/homeStack';
 
 export default function App() {

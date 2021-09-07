1f7ac6e17c4aa12ecb1585ac0387029775e45034
Kelvin <laikelvin8@gmail.com>
2021-09-07 15:38:23 +0100

    Added globalStyles.js

---
+++ screens/styles/globalStyles.js
@@ -0,0 +1,26 @@
+import {StyleSheet} from 'react-native'
+
+export default StyleSheet.create({
+    input: {
+        padding:10,
+        height:40,
+        marginBottom:20,
+        marginTop:20,
+        borderWidth:1,
+    
+   
+  },
+
+  item: {
+    padding:15,
+    borderRadius:5,
+    borderWidth:  1,
+    marginBottom: 15,
+    flexDirection: "row",
+    justifyContent:"space-between",
+
+
+
+},
+  
+  });

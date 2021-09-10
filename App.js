3cca71287dee0e4d7ff7bccace27e7706e2ec54f
Kelvin <laikelvin8@gmail.com>
2021-09-10 17:53:17 +0100

    Removed unused imported components

--- App.js
+++ App.js
@@ -1,14 +1,14 @@
-import React,{useState} from 'react';
-import { Button, SafeAreaView, StyleSheet, TextInput,Text } from 'react-native'
+import React from 'react';
 import Header from './components/Header';
 import Navigator from './routes/homeStack';
 import { NavigationContainer } from '@react-navigation/native';
 
+
 export default function App() {
   return (
     <NavigationContainer>
-    <Header title="ToDoApp"/>
-    <Navigator/>
+      <Header title="ToDoApp"/>
+      <Navigator/>
    </NavigationContainer>
   )
 }
